f42d767ee5baa0ee97f34685b7fb4cc2ce80f76e
Raphaël Londeix <raphael.londeix@celoxica.com>
2015-09-02 14:29:54 +0200

    Add multiple log levels.

--- src/dockerbot/log.py
+++ src/dockerbot/log.py
@@ -1,7 +1,40 @@
 from __future__ import print_function
 
+import os
 import sys
 
+class Level:
+    debug = 0
+    info = 1
+    warn = 2
+    error = 3
+
+LEVEL = Level.info
+
+def error(*args, **kw):
+    kw.setdefault('file', sys.stderr)
+    print('ERROR:', *args, **kw)
+
 def warn(*args, **kw):
+    if LEVEL > Level.warn:
+        return
     kw.setdefault('file', sys.stderr)
     print('WARNING:', *args, **kw)
+
+def status(*args, **kw):
+    if LEVEL > Level.info:
+        return
+    kw.setdefault('file', sys.stdout)
+    print('-- ', *args, **kw)
+
+def debug(*args, **kw):
+    if LEVEL > Level.debug:
+        return
+    kw.setdefault('file', sys.stderr)
+    print('DEBUG:', *args, **kw)
+
+
+def debug_stream():
+    if LEVEL > Level.debug:
+        return open(os.devnull, 'w')
+    return sys.stderr
